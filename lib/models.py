dcc5d43170fb4d32e156fca1b0a03d0ec0830120
Halim Choi <hchoicareer@gmail.com>
2023-05-25 17:30:59 -0400

    completed lab

--- lib/models.py
+++ lib/models.py
@@ -1,6 +1,8 @@
 from sqlalchemy import func
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.associationproxy import association_proxy
 
 Base = declarative_base()
 
@@ -8,14 +10,33 @@ class Game(Base):
     __tablename__ = 'games'
 
     id = Column(Integer(), primary_key=True)
-    title = Column(String())
-    genre = Column(String())
-    platform = Column(String())
-    price = Column(Integer())
-    created_at = Column(DateTime(), server_default=func.now())
-    updated_at = Column(DateTime(), onupdate=func.now())
+
+    reviews = relationship('Review', backref='game')
+    users = association_proxy('reviews', 'user',
+        creator=lambda us: Review(user=us))
 
     def __repr__(self):
-        return f'Game(id={self.id}, ' + \
-            f'title="{self.title}", ' + \
-            f'platform="{self.platform})"'
+        return f'Game(id={self.id})'
+
+class Review(Base):
+    __tablename__ = 'reviews'
+
+    id = Column(Integer(), primary_key=True)
+
+    game_id = Column(Integer(), ForeignKey('games.id'))
+    user_id = Column(Integer(), ForeignKey('users.id'))
+
+    def __repr__(self):
+       return f'Review(id={self.id})'
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer(), primary_key=True)
+
+    reviews = relationship('Review', backref='user')
+    games = association_proxy('reviews', 'game',
+        creator=lambda gm: Review(game=gm))
+
+    def __repr__(self):
+       return f'User(id={self.id})'
--- lib/seed.py
+++ lib/seed.py
@@ -1,32 +1,39 @@
 #!/usr/bin/env python3
 
-from faker import Faker
-import random
+from random import choice as rc
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from models import Game
+from models import Game, Review, User
 
-fake = Faker()
+engine = create_engine('sqlite:///seed_db.db')
+Session = sessionmaker(bind=engine)
+session = Session()
+
+def delete_records():
+    session.query(Game).delete()
+    session.query(Review).delete()
+    session.query(User).delete()
+    session.commit()
+
+def create_records():
+    games = [Game() for i in range(100)]
+    reviews = [Review() for i in range(1000)]
+    users = [User() for i in range(500)]
+    session.add_all(games + reviews + users)
+    session.commit()
+    return games, reviews, users
+
+def relate_records(games, reviews, users):
+    for review in reviews:
+        review.user = rc(users)
+        review.game = rc(games)
+
+    session.add_all(reviews)
+    session.commit()
 
 if __name__ == '__main__':
-    
-    engine = create_engine('sqlite:///seed_db.db')
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    # clear old data
-    session.query(Game).delete()
-    session.commit()
-
-    print("Seeding games...")
-    games = [
-        Game(
-            title=fake.name(),
-            genre=fake.word(),
-            platform=fake.word(),
-            price=random.randint(0,60)
-        )
-    for i in range(50)]
-    session.add_all(games)
-    session.commit()
+    delete_records()
+    games, reviews, users = create_records()
+    relate_records(games, reviews, users)
